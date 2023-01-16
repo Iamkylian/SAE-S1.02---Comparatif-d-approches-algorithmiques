379a0890cfcd99001002a40a74be5f0fd604c866
Iamkylian <kylian.gachet@etu.univ-tlse2.fr>
2023-01-16 20:03:28 +0100

    changement test

--- JeuDeLaVie.java
+++ JeuDeLaVie.java
@@ -1,4 +1,16 @@
 public class JeuDeLaVie{
     int nbL;
     int nbC;
+
+    JeuDeLaVie() throws Exception {
+        throw new Exception("Pas de paramètres");
+    }
+
+    JeuDeLaVie(int pfNbLignes, int pfNbColonnes) throws Exception {
+        if ((pfNbLignes <= 0) || (pfNbColonnes <= 0)) {
+            throw new Exception("Un des paramètres est vide ou négatif");
+        }
+        this.nbL = pfNbLignes;
+        this.nbC = pfNbColonnes;
+    }
 }
