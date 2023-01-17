8fc00270902c166373f7dfc7c046607fb79aa7d2
Iamkylian <kylian.gachet@etu.univ-tlse2.fr>
2023-01-17 15:22:15 +0100

    chg

--- ProgrammeJeuDeLaVie.java
+++ ProgrammeJeuDeLaVie.java
@@ -2,6 +2,7 @@ public class ProgrammeJeuDeLaVie {
     public static void main(String[] args) {
         try {
             JeuDeLaVie jeuV = new JeuDeLaVie(10, 10);
+
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
@@ -15,9 +16,32 @@ public class ProgrammeJeuDeLaVie {
     int tailleX;
     int tailleY;
 
-    public void afficher() {
-        System.out.println("Taille X: " + tailleX);
-        System.out.println("Taille Y: " + tailleY);
+    
+
+    public static creerJeuxDeLaVie() {
+        JeuDeLaVie jeuV = new JeuDeLaVie(10, 10);
+        jeuV.afficher();
     }
-    //bonjour
+    
+    public void afficher() {
+            System.out.println("Taille X: " + tailleX);
+            System.out.println("Taille Y: " + tailleY);
+        }
+    // La grille de cellules
+    boolean[][] grille;
+
+    // La taille de la grille (nombre de lignes et de colonnes)
+    int taille = 5;
+
+    // Initialise la grille avec une taille donnée et l'état initial des cellules
+    public static void JeuDeLaVie(int taille, boolean[][] etatInitial) {
+        JeuxDeLaVie grille = new boolean[taille][taille];
+
+        // Copie l'état initial dans la grille
+        for (int i = 0; i < taille; i++) {
+            for (int j = 0; j < taille; j++) {
+                grille[i][j] = etatInitial[i][j];
+            }
+        }
+    }
 }
