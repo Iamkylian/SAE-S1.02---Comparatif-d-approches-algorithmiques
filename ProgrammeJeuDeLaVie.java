ea1bd1c3c72edec0fb825afa138d1bae3035f69d
Iamkylian <kylian.gachet@etu.univ-tlse2.fr>
2023-01-17 15:44:10 +0100

    chg

--- ProgrammeJeuDeLaVie.java
+++ ProgrammeJeuDeLaVie.java
@@ -16,17 +16,6 @@ public class ProgrammeJeuDeLaVie {
     int tailleY = clavier.nextInt();
 
     /**
-     * Role : 
-     * 
-     * @param pftailleX : nombre de lignes
-     * @param pftailleY : nombre de colonnes
-     */
-    public ProgrammeJeuDeLaVie(int pftailleX, int pftailleY) {
-        this.tailleX = pftailleX;
-        this.tailleY = pftailleY;
-    }
-
-    /**
      * @param tailleX : nombre de lignes
      * @param tailleY : nombre de colonnes
      * @return : l'état initial des cellules
@@ -48,25 +37,26 @@ public class ProgrammeJeuDeLaVie {
     // La taille de la grille (nombre de lignes et de colonnes)
     int taille = 5;
 
-
-    // Initialise la grille avec une taille donnée et l'état initial des cellules
-
-
     /**
      * Rôle : Initialise la grille avec une taille donnée et l'état initial des
      * cellules
      * 
      * @throws Exception : si les paramètres sont négatifs ou nuls
      */
-    public static void creerJeuxDeLaVie(int tailleX, int tailleY) throws Exception {
-        if (tailleX == 0 || tailleY == 0) {
+    public static void creerJeuxDeLaVie(JeuDeLaVie m) throws Exception {
+        // Initialise la grille avec une taille donnée et l'état initial des cellules
+        if (m.tailleX == 0 || m.tailleY == 0) {
             throw new Exception("Pas de paramètres");
         }
         JeuDeLaVie jeuV = new JeuDeLaVie(10, 10);
+
         // Copie l'état initial dans la grille
-        for (int i = 0; i < taille; i++) {
-            for (int j = 0; j < taille; j++) {
-                grille[i][j] = etatInitial[i][j];
+
+        for (int i = 0; i < Jeutaille; i++) {
+
+            for (int j = 0; j < m.taille; j++) {
+
+                m.grille[i][j] = etatInitial[i][j];
             }
         }
         jeuV.afficher();
@@ -74,19 +64,28 @@ public class ProgrammeJeuDeLaVie {
 
     // Met à jour l'état de chaque cellule en fonction des règles du jeu
     public static void mettreAJour() {
+
         boolean[][] nouvelEtat = new boolean[taille][taille];
 
         for (int i = 0; i < taille ; i++) {
+
             for (int j = 0; j < taille ; j++) {
+
                 // Compte le nombre de cellules voisines vivantes
+
                 int voisinsVivants = 0;
+
                 for (int x = -1; x <= 1; x++) {
+
                     for (int y = -1; y <= 1; y++) {
+
                         if (x == 0 && y == 0) {
+
                             continue; // On ne compte pas la cellule elle-même
                         }
 
                         int l = (i + x + taille) % taille;
+
                         int c = (j + y + taille) % taille;
 
                         if (grille[l][c]) {
@@ -97,16 +96,22 @@ public class ProgrammeJeuDeLaVie {
 
                 // Applique les règles du jeu
                 if (grille[i][j] && (voisinsVivants == 2 || voisinsVivants == 3)) {
+
                     nouvelEtat[i][j] = true; // La cellule continue à vivre
+
                 } else if (!grille[i][j] && voisinsVivants == 3) {
+
                     nouvelEtat[i][j] = true; // La cellule naît
+
                 } else {
+
                     nouvelEtat[i][j] = false; // La cellule meurt
                 }
             }
         }
 
         // Copie le nouvel état dans la grille
+
         grille = nouvelEtat;
     }
 
