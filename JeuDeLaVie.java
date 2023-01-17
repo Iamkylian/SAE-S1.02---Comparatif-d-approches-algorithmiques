6981a7d099da6f4fcceaf616e141764e2c1a7ce2
Iamkylian <kylian.gachet@etu.univ-tlse2.fr>
2023-01-17 15:39:33 +0100

    chg

--- JeuDeLaVie.java
+++ JeuDeLaVie.java
@@ -1,26 +1,31 @@
 public class JeuDeLaVie{
-    int nbL;
-    int nbC;
+    int tailleX;
+    int tailleY;
 
     JeuDeLaVie() throws Exception {
-        this.nbL = 0;
-        this.nbC = 0;
-        if (this.nbL == 0 || this.nbC == 0) {
+        this.tailleX = 0;
+        this.tailleY = 0;
+        if (this.tailleX == 0 || this.tailleY == 0) {
             throw new Exception("Pas de paramètres");
         }
     }
         
 
-    JeuDeLaVie(int pfNbLignes, int pfNbColonnes) throws Exception {
-        if ((pfNbLignes <= 0) || (pfNbColonnes <= 0)) {
+    /**
+     * @param tailleX : nombre de lignes
+     * @param tailleY : nombre de colonnes
+     * @throws Exception : si les paramètres sont négatifs ou nuls
+     */
+    JeuDeLaVie(int tailleX, int tailleY) throws Exception {
+        if ((tailleX <= 0) || (tailleY <= 0)) {
             throw new Exception("Un des paramètres est vide ou négatif");
         }
-        this.nbL = pfNbLignes;
-        this.nbC = pfNbColonnes;
+        this.tailleX = tailleX;
+        this.tailleY = tailleY;
     }
 
     public void afficher() {
-        System.out.println("Nombre de lignes: " + this.nbL);
-        System.out.println("Nombre de colonnes: " + this.nbC);
+        System.out.println("Nombre de lignes: " + this.tailleX);
+        System.out.println("Nombre de colonnes: " + this.tailleY);
     }
 }
--- ProgrammeJeuDeLaVie.java
+++ ProgrammeJeuDeLaVie.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class ProgrammeJeuDeLaVie {
     public static void main(String[] args) {
         try {
@@ -8,40 +10,104 @@ public class ProgrammeJeuDeLaVie {
         }
     }
 
-    public ProgrammeJeuDeLaVie(int tailleX, int tailleY) {
-        this.tailleX = tailleX;
-        this.tailleY = tailleY;
+    Scanner clavier = new Scanner(System.in);
+
+    int tailleX = clavier.nextInt();
+    int tailleY = clavier.nextInt();
+
+    /**
+     * Role :
+     * 
+     * @param pftailleX : nombre de lignes
+     * @param pftailleY : nombre de colonnes
+     */
+    public ProgrammeJeuDeLaVie(int pftailleX, int pftailleY) {
+        this.tailleX = pftailleX;
+        this.tailleY = pftailleY;
     }
 
-    int tailleX;
-    int tailleY;
-
-    
-
-    public static creerJeuxDeLaVie() {
-        JeuDeLaVie jeuV = new JeuDeLaVie(10, 10);
-        jeuV.afficher();
+    /**
+     * @param tailleX : nombre de lignes
+     * @param tailleY : nombre de colonnes
+     * @return : l'état initial des cellules
+     */
+    public static int[][] etatInitial(int tailleX, int tailleY) {
+        int[][] etatInitial = new int[tailleX][tailleY];
+        return etatInitial;
     }
-    
+
     public void afficher() {
-            System.out.println("Taille X: " + tailleX);
-            System.out.println("Taille Y: " + tailleY);
-        }
+        System.out.println("Taille X: " + tailleX);
+        System.out.println("Taille Y: " + tailleY);
+    }
+
     // La grille de cellules
     boolean[][] grille;
+
 
     // La taille de la grille (nombre de lignes et de colonnes)
     int taille = 5;
 
-    // Initialise la grille avec une taille donnée et l'état initial des cellules
-    public static void JeuDeLaVie(int taille, boolean[][] etatInitial) {
-        JeuxDeLaVie grille = new boolean[taille][taille];
 
+    // Initialise la grille avec une taille donnée et l'état initial des cellules
+
+
+    /**
+     * Rôle : Initialise la grille avec une taille donnée et l'état initial des
+     * cellules
+     * 
+     * @throws Exception : si les paramètres sont négatifs ou nuls
+     */
+    public static void creerJeuxDeLaVie(int tailleX, int tailleY) throws Exception {
+        if (tailleX == 0 || tailleY == 0) {
+            throw new Exception("Pas de paramètres");
+        }
+        JeuDeLaVie jeuV = new JeuDeLaVie(10, 10);
         // Copie l'état initial dans la grille
         for (int i = 0; i < taille; i++) {
             for (int j = 0; j < taille; j++) {
                 grille[i][j] = etatInitial[i][j];
             }
         }
+        jeuV.afficher();
     }
+
+    // Met à jour l'état de chaque cellule en fonction des règles du jeu
+    public static void mettreAJour() {
+        boolean[][] nouvelEtat = new boolean[taille][taille];
+
+        for (int i = 0; i < taille ; i++) {
+            for (int j = 0; j < taille ; j++) {
+                // Compte le nombre de cellules voisines vivantes
+                int voisinsVivants = 0;
+                for (int x = -1; x <= 1; x++) {
+                    for (int y = -1; y <= 1; y++) {
+                        if (x == 0 && y == 0) {
+                            continue; // On ne compte pas la cellule elle-même
+                        }
+
+                        int l = (i + x + taille) % taille;
+                        int c = (j + y + taille) % taille;
+
+                        if (grille[l][c]) {
+                            voisinsVivants++;
+                        }
+                    }
+                }
+
+                // Applique les règles du jeu
+                if (grille[i][j] && (voisinsVivants == 2 || voisinsVivants == 3)) {
+                    nouvelEtat[i][j] = true; // La cellule continue à vivre
+                } else if (!grille[i][j] && voisinsVivants == 3) {
+                    nouvelEtat[i][j] = true; // La cellule naît
+                } else {
+                    nouvelEtat[i][j] = false; // La cellule meurt
+                }
+            }
+        }
+
+        // Copie le nouvel état dans la grille
+        grille = nouvelEtat;
+    }
+
 }
